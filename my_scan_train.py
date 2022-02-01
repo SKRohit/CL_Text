eb38dbfb510065da0d7b784641773e7d58740a1f
Rohit Kumar Singh <rohitku.singh8@gmail.com>
2022-02-01 19:58:54 +0530

    fixed scan training code

--- my_scan_train.py
+++ my_scan_train.py
@@ -1,25 +1,16 @@
+from accelerate import Accelerator, DistributedDataParallelKwargs
+from datasets import load_dataset
+from functools import partial
 import logging
 import os
-import sys
-from dataclasses import dataclass, field
-from typing import Optional, Union, List, Dict, Tuple
-import torch
 import pdb
-import random
-from functools import partial
+import sys
+import torch
 from torch.utils.data import DataLoader
-
-from accelerate import Accelerator, DistributedType
-
-from datasets import load_dataset
-import data_utils
-
 import transformers
 from transformers import (
     CONFIG_MAPPING,
-    MODEL_FOR_MASKED_LM_MAPPING,
     AutoConfig,
-    AutoModelForMaskedLM,
     AutoTokenizer,
     HfArgumentParser,
     AdamW,
@@ -27,47 +18,42 @@ from transformers import (
     set_seed,
     BertForPreTraining,
 )
-from transformers.tokenization_utils_base import BatchEncoding, PaddingStrategy, PreTrainedTokenizerBase
 from transformers.trainer_utils import is_main_process
-from transformers.file_utils import cached_property, torch_required, is_torch_available, is_torch_tpu_available
-from models import PretrainedSimCSEForCL, BertForCL, RobertaForCL, SCANClustering
+
+import data_utils
+from models import SCANClustering
 from arguments import ModelArguments, DataTrainingArguments, OurTrainingArguments
-from losses.losses import SimCSELoss, SCANLoss
+from losses.losses import SCANLoss
 
 
 MAX_GPU_BATCH_SIZE = 128
 logger = logging.getLogger(__name__)
 
+ddp_args = DistributedDataParallelKwargs()
+ddp_args.find_unused_parameters = True
+
 def training_fucntion(model_args, data_args, training_args):
-    accelerator = Accelerator(fp16=True, cpu=False)
+    accelerator = Accelerator(fp16=True, cpu=False,kwargs_handlers=[ddp_args])
     
     # Set seed before initializing model.
     set_seed(training_args.seed)
-    gradient_accumulation_steps=4
-    # In distributed training, the load_dataset function guarantee that only one local process can concurrently
-    # download the dataset.
-    data_files = {}
-    if data_args.train_file is not None:
-        data_files["train"] = data_args.train_file
-    datasets = load_dataset(data_args.train_file,)# cache_dir="./data/",)# delimiter="\t" if "tsv" in data_args.train_file else ",")
-    
-    
-    # Distributed training:
-    # The .from_pretrained methods guarantee that only one local process can concurrently
-    # download model & vocab.
+
+    #Loading Tokenizer
     config_kwargs = {
         "cache_dir": model_args.cache_dir,
         "revision": model_args.model_revision,
         "use_auth_token": True if model_args.use_auth_token else None,
     }
     if model_args.config_name:
-        config = AutoConfig.from_pretrained(model_args.config_name, **config_kwargs)
+        config = AutoConfig.from_pretrained(model_args.config_name,local_files_only=True, **config_kwargs)
     elif model_args.model_name_or_path:
-        config = AutoConfig.from_pretrained(model_args.model_name_or_path, **config_kwargs)
+        config = AutoConfig.from_pretrained(model_args.model_name_or_path,local_files_only=True, **config_kwargs)
     else:
         config = CONFIG_MAPPING[model_args.model_type]()
         logger.warning("You are instantiating a new config instance from scratch.")
 
+
+    #Loading Tokenizer
     tokenizer_kwargs = {
         "cache_dir": model_args.cache_dir,
         "use_fast": model_args.use_fast_tokenizer,
@@ -75,126 +61,126 @@ def training_fucntion(model_args, data_args, training_args):
         "use_auth_token": True if model_args.use_auth_token else None,
     }
     if model_args.tokenizer_name:
-        tokenizer = AutoTokenizer.from_pretrained(model_args.tokenizer_name, **tokenizer_kwargs)
+        tokenizer = AutoTokenizer.from_pretrained(model_args.tokenizer_name,local_files_only=True, **tokenizer_kwargs)
     elif model_args.model_name_or_path:
-        tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path, **tokenizer_kwargs)
+        tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path,local_files_only=True, **tokenizer_kwargs)
     else:
         raise ValueError(
             "You are instantiating a new tokenizer from scratch. This is not supported by this script."
             "You can do it from another script, save it, and load it from here, using --tokenizer_name."
         )
 
+
+    #Loading Model
     if model_args.model_name_or_path:
-        if 'san' in model_args.model_name_or_path:
-            model = SCANClustering(
-                config=config,
-                model_args=model_args
-            )
-            if model_args.do_mlm:
-                pretrained_model = BertForPreTraining.from_pretrained(model_args.model_name_or_path)
-                model.backbone.lm_head.load_state_dict(pretrained_model.cls.predictions.state_dict())
+        model = SCANClustering(
+            config=config,
+            model_args=model_args
+        )
+        # load pretrained-weights
+        if model_args.pretrained_weight_path:
+            state_dict = torch.load(model_args.pretrained_weight_path, map_location="cpu")
 
-        if 'simcse' in model_args.model_name_or_path:
-            model = PretrainedSimCSEForCL(
-                config=config,
-                model_args=model_args
-            )
-            if model_args.do_mlm:
-                pretrained_model = BertForPreTraining.from_pretrained(model_args.model_name_or_path)
-                model.lm_head.load_state_dict(pretrained_model.cls.predictions.state_dict())
+            if list(state_dict["model_state_dict"].keys())[0].startswith("module"):
+                from collections import OrderedDict
+                a = OrderedDict()
+                for k,v in state_dict["model_state_dict"].items():
+                    k = k[7:]
+                    a[k]=v
+                state_dict["model_state_dict"] = a
+                
+            model.backbone.load_state_dict(state_dict["model_state_dict"])
+    else:
+        raise Exception("Please provide model_name_or_path argument.")
 
-        elif 'roberta' in model_args.model_name_or_path:
-            model = RobertaForCL.from_pretrained(
-                model_args.model_name_or_path,
-                from_tf=bool(".ckpt" in model_args.model_name_or_path),
-                config=config,
-                cache_dir=model_args.cache_dir,
-                revision=model_args.model_revision,
-                use_auth_token=True if model_args.use_auth_token else None,
-                model_args=model_args                  
-            )
-        elif 'bert' in model_args.model_name_or_path:
-            model = BertForCL.from_pretrained(
-                model_args.model_name_or_path,
-                from_tf=bool(".ckpt" in model_args.model_name_or_path),
-                config=config,
-                cache_dir=model_args.cache_dir,
-                revision=model_args.model_revision,
-                use_auth_token=True if model_args.use_auth_token else None,
-                model_args=model_args
-            )
-            if model_args.do_mlm:
-                pretrained_model = BertForPreTraining.from_pretrained(model_args.model_name_or_path)
-                model.lm_head.load_state_dict(pretrained_model.cls.predictions.state_dict())
-        
-        
+
+    #Loading Dataset Containing Neighbours
+    text_col = data_args.text_col
+    # Prepare nbr dataset features
+    if data_args.dataset_name:
+        nbr_dataset = load_dataset(data_args.dataset_name)
+    else:
+        data_files = {k:v for k,v in zip(["train","validation","test"],[data_args.train_file,data_args.validation_file,data_args.test_file]) if v}
+        nbr_dataset = load_dataset("csv", data_files = data_files)
+    nbr_cnames = [cname for cname in nbr_dataset["train"].column_names if cname.startswith("nbr_")]
+    prepare_scan_features = partial(data_utils.prepare_scan_features, tokenizer=tokenizer,text_col=text_col)
+    for split in nbr_dataset:
+        nbr_dataset[split] = nbr_dataset[split].map(prepare_scan_features, batched=True, num_proc=2)
+
+
+    # Prepare aug dataset features
+    aug_args = {"num_aug":None, "augment":False,}
+    aug_dataset = {"train":None,"validation":None,"test":None}
+
+    #Loading Augmented Dataset
+    if data_args.use_augmentation:
+        if data_args.dataset_name:
+            aug_dataset = load_dataset(data_args.aug_dataset_name)
         else:
-            raise NotImplementedError
-    else:
-        logger.info("Training new model from scratch")
-        model = AutoModelForMaskedLM.from_config(config)
+            data_files = {k:v for k,v in zip(["train","validation","test"],[data_args.aug_train_file,data_args.aug_validation_file,data_args.aug_test_file]) if v}
+            aug_dataset = load_dataset("csv", data_files = data_files)
 
-    # model.resize_token_embeddings(len(tokenizer))
+        aug_args["num_aug"] = len([cname for cname in aug_dataset["train"].column_names if cname.startswith("aug_")])
+        aug_args["augment"] = True
 
-    # Prepare features
-    column_names = datasets["train"].column_names
-    if len(column_names) == 2:
-        # Pair datasets
-        sent0_cname = column_names[0]
-        sent1_cname = column_names[0]
-    elif len(column_names) == 1:
-        # Unsupervised datasets
-        sent0_cname = column_names[0]
-        sent1_cname = column_names[0]
-    else:
-        raise NotImplementedError
+        prepare_scan_features = partial(data_utils.prepare_scan_features, tokenizer=tokenizer,text_col=text_col,prefix="aug_")
+        for split in aug_dataset:
+            aug_dataset[split] = aug_dataset[split].map(prepare_scan_features, batched=True, num_proc=2)
 
-    prepare_cl_features = partial(data_utils.prepare_cl_features, tokenizer=tokenizer, sent0_cname=sent0_cname, sent1_cname=sent1_cname,)
-    my_cl_collator = partial(data_utils.my_cl_collator, tokenizer=tokenizer,mlm=model_args.do_mlm)
-    
-    train_dataset = datasets["train"].map(prepare_cl_features,batched=True,
-            num_proc=data_args.preprocessing_num_workers,
-            remove_columns=column_names,
-            load_from_cache_file=not data_args.overwrite_cache,
-        )
-    val_dataset = datasets["test"].map(prepare_cl_features,batched=True,
-            num_proc=data_args.preprocessing_num_workers,
-            remove_columns=column_names,
-            load_from_cache_file=not data_args.overwrite_cache,
-        )
 
-    train_dl = DataLoader(train_dataset,batch_size=data_args.batch_size, collate_fn=my_cl_collator)
-    val_dl = DataLoader(val_dataset,batch_size=data_args.batch_size*2, collate_fn=my_cl_collator)
+    my_train_scan_collator = partial(data_utils.my_scan_collator, tokenizer=tokenizer,nbr_in_data=len(nbr_cnames), 
+                                    num_nbr=data_args.num_nbr, sep_nbr=False,mlm=model_args.do_mlm,
+                                    aug_dataset= aug_dataset["train"],**aug_args)
+    my_validation_scan_collator = partial(data_utils.my_scan_collator, tokenizer=tokenizer,nbr_in_data=len(nbr_cnames), 
+                                    num_nbr=data_args.num_nbr, sep_nbr=False,mlm=model_args.do_mlm,
+                                    aug_dataset= aug_dataset["test"],**aug_args)
+
+    train_dl = DataLoader(nbr_dataset["train"],batch_size=data_args.batch_size, collate_fn=my_train_scan_collator)
+    val_dl = DataLoader(nbr_dataset["test"],batch_size=data_args.batch_size*2, collate_fn=my_validation_scan_collator)
 
     if data_args.batch_size > MAX_GPU_BATCH_SIZE:
-        gradient_accumulation_steps = data_args.batch_size // MAX_GPU_BATCH_SIZE
+        training_args.gradient_accumulation_steps = data_args.batch_size // MAX_GPU_BATCH_SIZE
         data_args.batch_size = MAX_GPU_BATCH_SIZE
+
 
     model = model.to(accelerator.device)
     optimizer = AdamW(params=model.parameters(), lr=training_args.learning_rate, correct_bias=True)
     scheduler = get_linear_schedule_with_warmup(optimizer, 
                     num_warmup_steps=100, 
-                    num_training_steps=(training_args.num_train_epochs * len(train_dl)//gradient_accumulation_steps),
+                    num_training_steps=(training_args.num_train_epochs * len(train_dl)//training_args.gradient_accumulation_steps),
                     )
-    loss_func = SimCSELoss(temp=model_args.temp, device=accelerator.device)
-
+    loss_func = SCANLoss(entropy_weight=model_args.entropy_weight,)
     model, optimizer, train_dl, val_dl = accelerator.prepare(model, optimizer, train_dl, val_dl)
 
+
     for epoch in range(int(training_args.num_train_epochs)):
+        
         accelerator.print(f"Training Epoch {epoch+1} Started.")
-        model.train()
         total_train_loss = 0
+
         for step, batch in enumerate(train_dl):
-            batch = {k:v.to(accelerator.device) for k,v in batch.items()}
-            outputs = model(**batch)
-            if model_args.do_mlm:
-                loss = loss_func(outputs.pooler_outut, outputs.mlm_logits,batch["mlm_labels"])
+            if training_args.keep_eval_mode:
+                model.eval()
             else:
-                loss = loss_func(outputs.pooler_output)
-            total_train_loss += loss.item()
-            loss = loss / gradient_accumulation_steps
-            accelerator.backward(loss)
-            if step % gradient_accumulation_steps == 0:
+                model.train()
+            # model.train()
+            batch = {k:v.to(accelerator.device) for k,v in batch.items()}
+            
+            if training_args.update_cluster_head_only:
+                with torch.no_grad():
+                    outputs = model(batch, forward_pass="backbone")
+                outputs = model(outputs.pooler_output,forward_pass="head")
+            else:
+                outputs = model(batch)
+            
+            # Loss for every head
+            total_loss = calculate_loss(loss_func, outputs)
+            total_loss = torch.sum(torch.stack(total_loss, dim=0))
+            total_loss = total_loss / training_args.gradient_accumulation_steps
+
+            total_train_loss += total_loss.item()
+            accelerator.backward(total_loss)
+            if step % training_args.gradient_accumulation_steps == 0:
                 accelerator.print(f"\rUpdating Weights after {step+1}/{len(train_dl)} steps.", end='', flush=True)
                 accelerator.clip_grad_norm_(model.parameters(),training_args.max_grad_norm)
                 optimizer.step()
@@ -202,19 +188,19 @@ def training_fucntion(model_args, data_args, training_args):
                 optimizer.zero_grad()
             
             torch.cuda.empty_cache()
-        ######## EVALUATIOn ON VALIDATION SET ###########
+            ######## EVALUATIOn ON VALIDATION SET ###########
             if (step+1)%training_args.eval_steps==0 or (step+1)==len(train_dl):
                 model.eval()
                 total_eval_loss=0
                 for step, batch in enumerate(val_dl):
                     batch = {k:v.to(accelerator.device) for k,v in batch.items()}
                     with torch.no_grad():
-                        outputs = model(**batch)
-                    if model_args.do_mlm:
-                        loss = loss_func(outputs.pooler_outut, outputs.mlm_logits,batch["mlm_labels"])
-                    else:
-                        loss = loss_func(outputs.pooler_output)
-                    total_eval_loss += loss.item()
+                        outputs = model(batch)
+                    # Loss for every head
+                    total_loss_eval = calculate_loss(loss_func, outputs)
+
+                    total_loss_eval = torch.min(torch.stack(total_loss_eval, dim=0))  # considering lowest loss out of the losses on different heads
+                    total_eval_loss += total_loss_eval.item()
                 
                 accelerator.print(f"epoch {epoch+1}: train_loss: {total_train_loss/len(train_dl)}, val_loss: {total_eval_loss/len(val_dl)},")
                 total_train_loss = 0
@@ -229,6 +215,14 @@ def training_fucntion(model_args, data_args, training_args):
                     }, f"{training_args.output_dir}/{epoch+1}_{total_eval_loss}.checkpoint.pth.tar")
                     data_args.best_eval_metric = total_eval_loss
                     accelerator.print("\n\n\n Best Model is being Saved \n\n\n")
+
+def calculate_loss(loss_func, outputs):
+    total_loss = []
+    for output in outputs:
+        length = output.size()[0]
+        total_loss_, consistency_loss_, entropy_loss_ = loss_func(output[:length//2],output[length//2:])
+        total_loss.append(total_loss_)
+    return total_loss
 
 if __name__ == "__main__":
 
